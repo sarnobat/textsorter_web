e35b9efded603d0113b971b790afe7c082fc0167
sarnobat <ss401533@gmail.com>
2013-12-21 11:14:26 -0800

    Initial commit

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
-open "index.html" || xdg-open "index.html"
+#open "index.html" || xdg-open "index.html"
 
 cd .groovy/lib
 mkdir -p ~/.groovy/lib
 cp *jar ~/.groovy/lib/
 cd ../..
-
-groovy webserver_json.groovy
+set -m
+groovy webserver_json.groovy &
+sleep 3
+open "index.html" || xdg-open "index.html"
+fg %1
