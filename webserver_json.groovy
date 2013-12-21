5c72c8b51e58cb34f7a5d589b3427e0cad590d78
sarnobat <ss401533@gmail.com>
2013-12-21 11:14:26 -0800

    Initial commit

--- webserver_json.groovy
+++ webserver_json.groovy
@@ -1,11 +1,15 @@
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.io.FileUtils;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.json.JSONException;
@@ -14,27 +18,31 @@ import org.json.JSONObject;
 import com.sun.net.httpserver.HttpServer;
 
 public class Server {
-        @Path("helloworld")
-        public static class HelloWorldResource { // Must be public
+	@Path("helloworld")
+	public static class HelloWorldResource { // Must be public
 
-                @GET
-                @Path("json")
-                @Produces("application/json")
-                public Response json(
-                	// TODO: @QueryParam("rootId") Integer iRootId
-                	) throws JSONException {
-                		System.out.println("1");
-                        JSONObject json = new JSONObject();
-                		System.out.println("2");
-                        json.put("foo", "bar");
-                		System.out.println("3");
-					    return Response.ok().header("Access-Control-Allow-Origin", "*")
-						 			.entity(json.toString()).type("application/json").build();
-                }
-        }
+		@GET
+		@Path("json")
+		@Produces("application/json")
+		public Response json(@QueryParam("filePath") String iFilePath) throws JSONException,
+				IOException {
+			System.out.println("1");
+			JSONObject json = new JSONObject();
+			System.out.println("2");
+			try {
+				String contents = FileUtils.readFileToString(new File(iFilePath));
+				json.put("foo", contents);
+				System.out.println("3");
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			return Response.ok().header("Access-Control-Allow-Origin", "*").entity(json.toString())
+					.type("application/json").build();
+		}
+	}
 
-        public static void main(String[] args) throws URISyntaxException {
-                HttpServer server = JdkHttpServerFactory.createHttpServer(
-                                new URI("http://localhost:9099/"), new ResourceConfig(HelloWorldResource.class));
-        }
+	public static void main(String[] args) throws URISyntaxException {
+		HttpServer server = JdkHttpServerFactory.createHttpServer(
+				new URI("http://localhost:9099/"), new ResourceConfig(HelloWorldResource.class));
+	}
 }
