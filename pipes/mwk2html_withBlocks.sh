e257ebd2b30481084ca6d1851d35e33a2740655a
ssarnobat <ss401533@gmail.com>
2017-03-15 00:01:33 -0700

    Cron daily autocommit

--- pipes/mwk2html_withBlocks.sh
+++ pipes/mwk2html_withBlocks.sh
@@ -1,6 +1,8 @@
 cat - \
   | perl -pe 's{^==\s+(.*)\s+==}{<h2>$1</h2>}g' \
+  | perl -pe 's{^==\s+==}{<h2>(blank)</h2>}g' \
   | perl -pe 's{^===\s+(.*)\s+===}{<h3>$1</h3>}g' \
+  | perl -pe 's{^===\s+===}{<h3>(blank)</h3>}g' \
   | perl -pe 's{^====\s+(.*)\s+====}{<h4>$1</h4>}g' \
   | perl -pe 's{^=====\s+(.*)\s+=====}{<h5>$1</h5>}g' \
   | perl -pe 's{\n}{<br>\n}g'
